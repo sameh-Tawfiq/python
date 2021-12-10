adcbb184819c3ec3a43bc6bf679b17931902553e
root <root@dev-python.local>
2021-12-10 18:41:10 +0200

    creating_classes.py is created

---
+++ creating_classes.py
@@ -0,0 +1,11 @@
+class Car:
+    """
+    Docsring describing the class
+    """
+    def __init__(self, engine, tires):
+        """
+        Docstring describing the method
+        """
+        self.engine = engine
+        self.tires = tires
+
